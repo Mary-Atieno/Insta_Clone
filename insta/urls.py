cd397f6fbcfa1bc56475d364da9b56527730d4c0
mary atieno <mercymary 948@gmail.com>
2022-06-08 16:08:58 +0300

    fix url bug

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from insta import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from insta.views import UserProfile, EditProfile
 
 urlpatterns = [
+    #  profile section
+    # path('', views.index, name="index"),
+    path('profile/edit', EditProfile, name="editprofile"),
 
-    path('', views.index, name="index"),
+    # User Authentication
+    path('sign-up/', views.register, name="sign-up"),
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign-in.html", redirect_authenticated_user=True), name='sign-in'),
+    path('sign-out/', auth_views.LogoutView.as_view(template_name="sign-out.html"), name='sign-out'), 
+
 ]
